50a82be97145357047160baaacf0ef3ef1e1ba34
Abrams <abramsi@ggclinic.com>
2020-01-04 11:56:48 -0600

    added api key

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // https://api.github.com/users/izaacAbrams/repos
-
+const apiKey = "252ce56616572cd060fc33ed361953e6e21797a7";
 
 function getRepos(ghHandle) {
     const options = {
     headers: new Headers({
-        key: " " })
+        "token": apiKey,
+        "Access-Control-Allow-Origin": `*`})
     };
     fetch(`https://api.github.com/users/${ghHandle}/repos`, options)
         .then(response => {
